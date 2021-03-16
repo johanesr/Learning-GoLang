dfcb612bd7e51660fee560156ffba4244a5350cd
ronaldo97 <johanesronaldo@gmail.com>
2021-03-16 16:03:38 +0700

    Database MySQL Connection

--- go.mod
+++ go.mod
@@ -5,5 +5,6 @@ go 1.16
 require (
 	github.com/alexedwards/scs/v2 v2.4.0 // indirect
 	github.com/go-chi/chi/v5 v5.0.1 // indirect
+	github.com/go-sql-driver/mysql v1.5.0 // indirect
 	github.com/justinas/nosurf v1.1.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
@@ -8,11 +9,17 @@ import (
 
 	"github.com/alexedwards/scs/v2"
 	"github.com/johanesr/jo_todo/config"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 const port_number = ":8080"
 var app config.AppConfig
 var session *scs.SessionManager
+
+type testDB struct {
+	Id 		int
+	Name 	sql.NullString
+}
 
 func main() {
 	app.IsProd = false
@@ -29,6 +36,27 @@ func main() {
 	app.Session = session
 	// End session setup
 
+	fmt.Println("Connecting to a MySQL Databse")
+	db, err := sql.Open("mysql", "root:password@tcp(127.0.0.1:3306)/student")
+	if err!=nil {log.Fatal("An error when connecting to mysql database: ",err)}
+	defer db.Close()
+	fmt.Println("Successfully connected to a MySQL Database!")
+
+	//insert, err := db.Query("INSERT INTO student VALUES (6,'go', 'Computer Science', 1)")
+	//if err!=nil {log.Fatal("An error when inserting to mysql database: ",err)}
+	//defer insert.Close()
+	//fmt.Println("Successfully inserted to a MySQL Database!")
+
+	results, err := db.Query("SELECT student_id, student_name FROM student")
+	if err!=nil {log.Fatal("An error when reading from mysql database: ",err)}
+	for results.Next() {
+		var user testDB
+
+		// Scan have to pass a number of arguments = number of column return
+		err := results.Scan(&user.Id, &user.Name)
+		if err!=nil {log.Print("fatal: ",err)}
+		fmt.Printf("ID: %d, name: %s\n",user.Id,user.Name.String)
+	}
 
 	srv := &http.Server {
 		Addr: port_number,
